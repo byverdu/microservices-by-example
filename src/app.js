acab40a0d92ec1fbe63144352a38be9f6cbec5fb
Albert Vallverdu <byverdu@gmail.com>
2023-09-11 13:49:56 +0100

    nodemon setup + video endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import fs, { promises } from 'fs';
+import path from 'path';
+
+const { stat } = promises;
 
 const app = express();
 
 app.get('/', async (_, res) => {
   res.send('hello world');
+});
+
+app.get('/video', async (_, res) => {
+  const videoPath = path.resolve('.', 'src/videos/sample.mp4');
+
+  try {
+    const fileSize = await stat(videoPath);
+
+    res.writeHead(200, {
+      'Content-Length': fileSize.size,
+      'Content-Type': 'video/mp4',
+    });
+
+    fs.createReadStream(videoPath).pipe(res);
+  } catch (e) {
+    res.send(String(e));
+  }
 });
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import 'dotenv/config.js';
+
 import { app } from './app.js';
 
-const PORT = process.env.PORT || 3000;
+if (!process.env.PORT) {
+  throw new Error('No PORT env has being set, please provide one');
+}
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Application running on port ${PORT}`));
