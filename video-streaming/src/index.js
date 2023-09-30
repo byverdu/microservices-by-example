92cae572368231d1bfca6e69cc7622df933cd5e9
Albert Vallverdu <byverdu@gmail.com>
2023-09-30 22:48:10 +0100

    testing git hook

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -2,7 +2,11 @@ import { app } from './app.js';
 
 const { PORT, VIDEO_STORAGE_HOST, VIDEO_STORAGE_PORT } = process.env;
 
-for (const env of Object.keys({ PORT, S3_BUCKET_REGION, S3_BUCKET_NAME })) {
+for (const env of Object.keys({
+  PORT,
+  VIDEO_STORAGE_HOST,
+  VIDEO_STORAGE_PORT,
+})) {
   if (!process.env[env]) {
     throw new Error(`No ${env} env has being set, please provide one.`);
   }
