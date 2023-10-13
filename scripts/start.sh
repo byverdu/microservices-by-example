78947fbc4b3a739b2eab404c7544c23da7927731
Albert Vallverdu <byverdu@gmail.com>
2023-10-13 23:09:29 +0100

    Add path to volumes

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,13 +4,14 @@
 
 TEMP=$1
 MODE=$(echo "${TEMP:-prod}" | tr '[:lower:]' '[:upper:]')
+ERROR_MSG='You need to use debug or dev or blank'
 
 if [[ ${MODE} != 'PROD' ]] && [[ ${MODE} != 'DEV' ]] && [[ ${MODE} != 'DEBUG' ]]; then
-    echo 'You need to use debug or dev or blank'
-    exit 1
+    echo "${ERROR_MSG}"
+    exit 0
 fi
 
-echo -e "\033[0;32m Starting application in ${MODE} mode \033[0m\n"
+echo -e "\033[0;32m Starting application in ${MODE} mode. \033[0m\n"
 
 case ${MODE} in
 DEV)
@@ -23,6 +24,6 @@ PROD)
     docker compose --env-file=./.env up --build
     ;;
 *)
-    echo 'You need to use debug or dev or blank'
+    echo "${ERROR_MSG}"
     ;;
 esac
